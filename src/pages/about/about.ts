00fc8d4b1287e065f49f808480b4769763036cda
DeveloperWakeling <developerwakeling@gmail.com>
2017-08-24 23:06:55 -0400

    Changing names of folders and files, setup comments of where things need to go and how they should flow

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -5,10 +5,10 @@ import { NavController, NavParams } from 'ionic-angular';
 import { ItemDetailsPage } from '../item-details/item-details';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+  selector: 'page-about',
+  templateUrl: 'about.html'
 })
-export class ListPage {
+export class AboutPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
